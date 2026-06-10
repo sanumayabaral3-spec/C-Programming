742f3112056569a81235ca12bab0327b51902cc3
Prashan Baral B.K. <sanumayabaral3@gmail.com>
2026-06-10 23:25:31 +0545

    Add files via upload

---
+++ LAB 1/2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter first Number:");
+	scanf("%d",&a);	
+	printf("Enter Second Number:");
+	scanf("%d",&b);	
+	c=a+b;
+	printf("%d is the sum of Those Number.",c);
+	return 0;
+}
