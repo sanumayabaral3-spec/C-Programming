aac85103f18d54de6131135716e8b9f241de1647
Prashan Baral B.K. <sanumayabaral3@gmail.com>
2026-06-24 22:18:33 +0545

    Lab 1 Question number 11 committed.

---
+++ LAB 1/11.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter value of a:");
+	scanf("%d",&a);
+ 	printf("Enter value of b:")	;
+ 	scanf("%d",&b);
+ 	a=a;
+ 	b=b;
+ 	printf("Value of a and b after swapping: \n");
+ 	printf("Value of a= %d \n",b);
+ 	printf("Value of b= %d",a);
+ 	return 0;
+}
