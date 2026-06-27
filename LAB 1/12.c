430d741abd5794ad490dd2e5874a71c3d7356496
Prashan Baral B.K. <sanumayabaral3@gmail.com>
2026-06-27 21:05:15 +0545

    Lab 1 question number 12 committed.

---
+++ LAB 1/12.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter a :");
+	scanf("%d",&a);
+	printf("Enter b :");
+	scanf("%d",&b);
+	if(a<b)
+	printf("a is smaller");
+	else
+	printf("b is smaller");
+	return 0;
+}
