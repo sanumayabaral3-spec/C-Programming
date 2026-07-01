beb8ab1373d4c09c8769410b8f6422a4dfd81368
Prashan Baral B.K. <sanumayabaral3@gmail.com>
2026-07-01 21:51:30 +0545

    Lab 1 Question Number 14 committed.

---
+++ LAB 1/14.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter Your Number:");
+	scanf("%d",&a);
+	b=a<<2;
+	printf("The Number after shift data left by two bits: %d",b);
+	return 0;
+}
