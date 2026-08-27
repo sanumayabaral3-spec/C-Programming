06da3537e57a3695433d17055e3f3846208c6583
Prashan Baral B.K. <sanumayabaral3@gmail.com>
2026-08-28 00:08:15 +0545

    Lab 2 Question number 1 commited.

---
+++ Lab 2/1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter first number:");
+	scanf("%d",&a);
+	printf("Enter second number:");
+	scanf("%d",&b);
+	if(a>b)
+	printf("%d is maximum.",a);
+	else
+	printf("%d is maximum.",b);
+	return 0;	
+}
